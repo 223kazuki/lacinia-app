54b250d6ad4ecd35d5b1c283d1eb243860ab2cba
Kazuki Tsutsumi <rixi223.kazuki@gmail.com>
2017-12-08 11:04:29 -0800

    Added cors.

--- src/lacinia_app/pedestal.clj
+++ src/lacinia_app/pedestal.clj
@@ -1,10 +1,18 @@
 (ns lacinia-app.pedestal
-  (:require [io.pedestal.http :as http]
+  (:require [integrant.core :as ig]
             [com.walmartlabs.lacinia.pedestal :as pedestal]
-            [integrant.core :as ig]))
+            [io.pedestal.http :as http]
+            [io.pedestal.http.route :as route]
+            [io.pedestal.http.cors :refer [allow-origin]]))
 
-(defmethod ig/init-key :lacinia-app/pedestal [_ {:keys [schema]}]
-  (let [service (pedestal/service-map schema {:graphiql true})
+(defmethod ig/init-key :lacinia-app/pedestal [_ {:keys [schema graphiql]}]
+  (let [options {:graphiql (true? graphiql)}
+        preflight-route ["/graphql" :options (fn [_] {:status 200}) :route-name ::preflight]
+        routes (as-> (pedestal/graphql-routes schema options) $
+                 (conj $ preflight-route)
+                 (route/expand-routes $)
+                 (map (fn [route] (update-in route [:interceptors] (partial cons (allow-origin {:creds true :allowed-origins some?})))) $))
+        service (pedestal/service-map schema (assoc options :routes routes))
         server (atom (http/create-server service))]
     (http/start @server)
     server))
